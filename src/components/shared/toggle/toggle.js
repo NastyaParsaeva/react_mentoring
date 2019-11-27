744e3547dd9277e9d8b274bf09eea484fd84ebad
Anastasiia Parsaeva <anastasiia_parsaeva@epam.com>
2019-11-27 22:06:29 +0400

    toggle component fixed

--- src/components/shared/toggle/toggle.js
+++ src/components/shared/toggle/toggle.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './toggle.css';
 
 const Toggle = (options) => {
   return (
     <div className="toggle">
-      <p className="toggle__name">{options.toggleName}</p>
+      <p className="toggle__name">{options.options.toggleName}</p>
       <div className="toggle__item-list">
-        {(options.toggleItems && options.toggleItems.length) ? 
-          options.toggleItems.map((item) => 
-            <p className="toggle__item" key={item}>{item}</p>
-          ) : 
-          ''}
+        {options.options.toggleItems.map((item) => (
+          <p
+            className={"toggle__item" + (item.selected ? ' selected' : '')}
+            key={item.name}
+          >{item.name}</p>
+        )
+        )}
       </div>
     </div>
   );
 };
 
-export default Toggle;
+export default Toggle
